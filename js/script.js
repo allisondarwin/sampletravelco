c2e181d37d1cac0bc4e99b6d2b397ca6531b3b1f
Allison Darwin <allisonpalmer07@gmail.com>
2020-03-06 12:16:50 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){ //start of jquery wrapper
+
+  $(function() {
+      $(".toggle").on("click", function() {
+          if ($(".item").hasClass("active")) {
+              $(".item").removeClass("active");
+              $(this).find("a").html("<i class='fas fa-bars'></i>");
+          } else {
+              $(".item").addClass("active");
+              $(this).find("a").html("<i class='fas fa-times'></i>");
+          }
+      });
+    });
+
+}); //end of jquery wrapper
